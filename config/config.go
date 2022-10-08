4578f66d61ad5fbe654b16009c3aad315b5d4b4a
aichy126 <work@aichy.com>
2022-10-08 16:03:18 +0800

    add consul

--- config/config.go
+++ config/config.go
@@ -1,9 +1,12 @@
 package config
 
 import (
+	"bytes"
 	"flag"
+	"fmt"
 	"os"
 
+	consulapi "github.com/hashicorp/consul/api"
 	"github.com/spf13/viper"
 )
 
@@ -32,23 +35,60 @@ func NewConfig(ConfigPath string) (*Config, error) {
 		return Conf, err
 	}
 	Conf.Viper = localConfig
+	consulConfig, exist, err := getConsulConf(localConfig)
+	if err != nil {
+		return Conf, err
+	}
+	if exist {
+		Conf.Viper = consulConfig
+	}
 	return Conf, nil
 }
 
-func InitConfig(ConfigFilePath string) (Config *viper.Viper, err error) {
-	// another chance to read the config file from environment variable
-	if ConfigFilePath == "" {
-		ConfigFilePath = os.Getenv(EnvConfigPath)
+func getConsulConf(config *viper.Viper) (*viper.Viper, bool, error) {
+	consulConfig := viper.New()
+	consulConfig.SetConfigType("toml")
+	exist := config.IsSet("config")
+	if !exist {
+		return consulConfig, exist, nil
 	}
+	address := config.GetString("config.address")
+	key := config.GetString("config.key")
+	if len(address) > 0 && len(key) > 0 {
+		newConfigClient(address)
+		t, err := GetByTree(key)
+		if err != nil {
+			return consulConfig, exist, err
+		}
+		err = consulConfig.ReadConfig(bytes.NewBuffer(t))
+		if err != nil {
+			return consulConfig, exist, err
+		}
+	}
+	return consulConfig, exist, nil
+}
 
-	localConfig := viper.New()
-	localConfig.SetConfigFile(ConfigFilePath)
-	err = localConfig.ReadInConfig() // Find and read the config file
-	if err != nil {                  // Handle errors reading the config file
-		return Config, err
+var client *consulapi.Client
+
+func newConfigClient(address string) {
+	config := consulapi.DefaultConfig()
+	config.Address = address
+	c, err := consulapi.NewClient(config)
+	if err != nil {
+		panic(fmt.Sprintf("consul client error : %+v\n ", err))
 	}
-	Config = localConfig
-	return Config, nil
+	client = c
+}
+
+func GetByTree(key string) ([]byte, error) {
+	kvPair, _, err := client.KV().Get(key, &consulapi.QueryOptions{})
+	if err != nil {
+		return nil, err
+	}
+	if kvPair == nil {
+		return nil, fmt.Errorf("consul key not found:%s", key)
+	}
+	return kvPair.Value, nil
 }
 
 // Get the local config file path from the command line
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/goioc/di v1.7.1
+	github.com/hashicorp/consul/api v1.12.0
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/viper v1.12.0
@@ -16,8 +17,10 @@ require (
 )
 
 require (
+	github.com/armon/go-metrics v0.3.10 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -25,11 +28,19 @@ require (
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v1.2.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hashicorp/serf v0.9.7 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
