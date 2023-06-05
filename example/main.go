05e3ed5820f9700ef7e148f5aa99c1fe3a159c92
aichy126 <16996097+aichy126@users.noreply.github.com>
2023-06-05 16:39:03 +0800

    增加使用例子

--- example/main.go
+++ example/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"github.com/aichy126/igo"
 	"github.com/aichy126/igo/util"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	igo.App = igo.NewApp("") //初始化各个组件
 	debug := util.ConfGetbool("local.debug")
 	util.CDump(debug)
+	Router(igo.App.Web.Router) //引入 gin路由
+	igo.App.Web.Run()
+}
 
+func Router(r *gin.Engine) {
+	r.GET("ping", Ping)
+}
+
+func Ping(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "Hello World."})
 }
